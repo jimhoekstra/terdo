220bb3425ff81b16a65885c18021d2a8c5ca8a00
Jim Hoekstra <jimhoekstra2@gmail.com>
2025-03-12 20:58:06 +0100

    enable saving of notes

--- components/note.py
+++ components/note.py
@@ -9,19 +9,42 @@ class Note(Widget):
     content: reactive[Path] = reactive(Path.cwd() / "markdown" / "Do groceries.md")
     can_focus = True
 
-    BINDINGS = [("e", "edit", "Edit")]
+    BINDINGS = [
+        ("e", "edit", "Edit"),
+        ("ctrl+s", "save", "Save"),
+        ("escape", "save", "Save"),
+    ]
 
     def compose(self) -> ComposeResult:
         yield Markdown("", id="note-viewer")
+        yield TextArea("", language="markdown", id="note-editor", classes="hidden")
 
     async def watch_content(self) -> None:
+        await self.reload_content()
+
+    async def reload_content(self) -> None:
         markdown_element = self.query_one("#note-viewer", Markdown)
         await markdown_element.load(self.content)
 
     async def action_edit(self) -> None:
-        self.remove_children()
-        self.mount(
-            TextArea(
-                text=self.content.read_text(), language="markdown", id="note-editor"
-            )
-        )
+        markdown_element = self.query_one("#note-viewer", Markdown)
+        markdown_element.add_class("hidden")
+
+        textarea_element = self.query_one("#note-editor", TextArea)
+        textarea_element.remove_class("hidden")
+
+        if textarea_element.text != self.content.read_text():
+            textarea_element.text = self.content.read_text()
+        textarea_element.focus()
+
+    async def action_save(self) -> None:
+        markdown_element = self.query_one("#note-viewer", Markdown)
+        markdown_element.remove_class("hidden")
+
+        textarea_element = self.query_one("#note-editor", TextArea)
+        textarea_element.add_class("hidden")
+
+        self.content.write_text(textarea_element.text)
+        await self.reload_content()
+
+        self.focus()
